680b514398d81e9895abf70856575ef40c4917d1
aya effoua <carineeffoua@gmail.com>
2023-05-31 17:50:58 -0400

    contact hover

--- src/components/sub/navigation.js
+++ src/components/sub/navigation.js
@@ -1,46 +1,45 @@
-import React,{useState} from "react"
-import "./navigation.css"
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import "./navigation.css";
 
 const Navigation = () => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-const toggleNavigation=()=>{
+  const [isOpen, setIsOpen] = useState(false);
+  const [activeLink, setActiveLink] = useState('/portfolio'); 
 
+  const toggleNavigation = () => {
     setIsOpen(!isOpen);
-    };
-    return (
-        <div>
-        {!isOpen ? (
-            <button className="hamburger" onClick={toggleNavigation}>
-            &#9776;
-            </button>
+  };
+
+  return (
+    <div>
+      {!isOpen ? (
+        <button className="hamburger" onClick={toggleNavigation}>
+          &#9776;
+        </button>
+      ) : null}
+      <nav className={`menu ${isOpen ? "show" : ""}`}>
+        {isOpen ? (
+          <button className="close" onClick={toggleNavigation}>
+            X
+          </button>
         ) : null}
-        <nav className={`menu ${isOpen ? "show" : ""}`}>
-            {isOpen ? (
-            <button className="close" onClick={toggleNavigation}>
-                X
-            </button>
-            ) : null}
-            <ul>
-                
-            <li>
-                <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-                <Link to="/resume">Resume</Link>
-            </li>
-            <li>
-                <Link to="/contact">Contacts</Link>
-            </li>
-            <li>
-                <Link to="/about">About me</Link>
-            </li>
-            </ul>
-        </nav>
-        </div>
-
-      );
-
+        <ul>
+          <li>
+            <Link to="/portfolio" className={activeLink === '/portfolio' ? 'active' : ''} onClick={() => setActiveLink('/portfolio')}>Portfolio</Link>
+          </li>
+          <li>
+            <Link to="/resume" className={activeLink === '/resume' ? 'active' : ''} onClick={() => setActiveLink('/resume')}>Resume</Link>
+          </li>
+          <li>
+            <Link to="/contact" className={activeLink === '/contact' ? 'active' : ''} onClick={() => setActiveLink('/contact')}>Contacts</Link>
+          </li>
+          <li>
+            <Link to="/about" className={activeLink === '/about' ? 'active' : ''} onClick={() => setActiveLink('/about')}>About me</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
-export default Navigation
+
+export default Navigation;
